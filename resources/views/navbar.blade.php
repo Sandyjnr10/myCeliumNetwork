2e7458a9d42d509cef17efb8e9ad41e6c26e9117
sandy <sandy@Sandys-MacBook.local>
2023-02-17 10:39:35 +0000

    sandy addition

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,30 @@
+{{-- <ul class="nav nav-center">
+  <li class="nav-item">
+    <a class="nav-link" href="/">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/about"> Link To Social Media URL</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/about"> Dashboard</a>
+  </li>
+</ul> --}}
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/dashboard">Dashboard</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    {{-- <div class="collapse navbar-collapse" id="navbarNav"> --}}
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">  Link to Social URL</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
